4db4511924cd5166015103e9fcfd416ef892c649
Liam Daniel Hurt <liamhurt51@gmail.com>
2016-06-23 10:39:12 -0700

    DOMexercise
    a small DOM exercise

---
+++ JavaScript-Exersices/DOMmain.js
@@ -0,0 +1,10 @@
+document.querySelector('article').querySelector('h1');
+document.getElementsByTagName('article')[0].querySelector('h1');
+var tittle = document.querySelector('#article-1').getElementsByTagName('h1')[0];
+document.getElementById('article-1').querySelector('h1');
+
+tittle.innerHTML = 'This is a tittle';
+
+tittle.classList.toggle('.article-heading')
+
+document.querySelector('.btn-get-it').href = 'http://www.google.com'
