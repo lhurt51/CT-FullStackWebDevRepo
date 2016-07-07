2070fca9ef870db47441230b8bc4d892c463144f
Liam Hurt <liamhurt51@gmail.com>
2016-07-07 16:23:36 -0700

    easier way

--- Ruby-Exercises/FirstDayExercise.rb
+++ Ruby-Exercises/FirstDayExercise.rb
@@ -2,7 +2,7 @@ my_cars = []
 car1 = {make: 'Ferarri', model:'Enzo', color:'Blue'}
 car2 = {make: 'Ferarri', model:'Spider', color:'Green'}
 car3 = {make: 'Buggatti', model:'Veyron', color:'Orange'}
-car4 = {make: 'Lamborguini', model:'Mercilago', color:'Black'}
+car4 = {make: 'Lamborghini', model:'Murcielago', color:'Black'}
 car5 = {make: 'Mclaren', model:'P1', color:'Red'}
 
 my_cars.push(car1, car2, car3, car4, car5)
@@ -27,4 +27,4 @@ array_string = []
 for car in my_cars
   array_string.push("#{car[:make]} #{car[:model]} #{car[:color]}")
 end
-puts "#{array_string[0]} | #{array_string[1]} | #{array_string[2]} | #{array_string[3]} | #{array_string[4]}"
+puts array_string.join(' | ')
